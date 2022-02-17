d19132881444141573886acf4ce80ac847489f2e
Garima Pandey <59961721+GarimaPandey4all@users.noreply.github.com>
2022-02-17 20:18:00 +0530

    Add files via upload

---
+++ JQuery/Jquery.js
@@ -0,0 +1,66 @@
+$("button").on("click", function(){
+    // $("h1").animate({opacity:0.5});
+    $("h1").slideUp().slideDown().animate({opacity:0.5});
+})
+
+// $("button").on("click", function(){
+//     //$("h1").fadeIn();
+//     //$("h1").fadeOut();
+//    $("h1").fadeToggle();
+// })
+
+// $("button").on("click", function(){
+//      //$("h1").slideUp();
+//     // $("h1").slideDown();
+//      $("h1").slideToggle();
+// })
+
+// $("button").on("click", function(){
+//     // $("h1").hide();
+//     // $("h1").show();
+//     $("h1").toggle();
+// })
+
+
+// $("h1").before("<button>New</button>");
+// $("h1").after("<button>New</button>");
+// $("h1").append("<button>New</button>");
+// $("h1").prepend("<button>New</button>");
+
+// $("h1").on("mouseover", function(){
+//     $("h1").css("color", "red");
+// })
+
+// $("button").remove();
+
+// $("button").html("Don't Click Me");
+
+// JS Syntax
+// for(var i = 0; i < 4; i++)
+// {
+//     document.querySelectorAll("button")[i].addEventListener('click', function(){
+//         document.querySelector("h1").style.color = "yellow";
+//     })
+// }
+
+// Jquery Syntax
+// $("button").on("click", function(){
+//     $("h1").css('color', 'yellow');
+// })
+
+
+// $("h1").addClass("big-title");
+
+// $("h1").on("click", function(){
+//     $("h1").css('font-size', '100px');
+// })
+
+// $("h1").on("click", function(){
+//     $("h1").css('color', 'red');
+// })
+
+// $("h1").click(function(){
+//     $("h1").css('color', 'red');
+// });
+
+// $("h1").css('color', 'red');
